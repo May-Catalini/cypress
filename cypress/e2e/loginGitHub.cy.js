a7f59786d1639b9050e8298718dba055bc417ca5
Mailen Catalini <maycat_99@hotmail.com>
2025-04-13 23:30:30 -0300

    Agregando fixture

--- cypress/e2e/loginGitHub.cy.js
+++ cypress/e2e/loginGitHub.cy.js
@@ -2,27 +2,36 @@
 
 describe('Ingresar a Github', () => {
 
-    const selectorEmail = '#login_field';
-    const selectorPassword = '#password';
-    const selectorBtn = '.position-relative > .btn'; 
-    
     const url = Cypress.env('url');
     const email = Cypress.env('email');
     const contraseña = Cypress.env('contraseña');
     const usuario_GitHub = Cypress.env('usuario_GitHub');
 
+    let gitHub;
 
     beforeEach(() => {
-        cy.login(url, selectorEmail, selectorPassword, selectorBtn, email, contraseña); 
+
+        cy.fixture('gitHub').then((data) => {
+            gitHub = data;
+
+            cy.login(
+                url,
+                gitHub.selectorEmail,
+                gitHub.selectorPassword,
+                gitHub.selectorBtn,
+                email,
+                contraseña
+            );
+        });
     });
 
     it('verificar ingreso a Github y usuario', () => {
 
         cy.url().should('include', 'github.com'); 
 
-        cy.get('.Button-label > .avatar').should('be.visible').click();
+        cy.get(gitHub.avatar).should('be.visible').click();
 
-        cy.get('.text-bold > .Truncate__StyledTruncate-sc-23o1d2-0')
+        cy.get(gitHub.usuarioTexto)
             .should('contain.text', usuario_GitHub)
             .then(() => {
                 cy.log('Usuario verificado correctamente ✅');
